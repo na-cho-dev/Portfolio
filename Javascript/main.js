62bd65639e8970a1777c80cd9ac34d1710837fd1
Fortune Iheanacho <nachodev369@gmail.com>
2022-06-30 20:39:39 +0100

    Portfolio Updated V3

--- Javascript/main.js
+++ Javascript/main.js
@@ -102,6 +102,22 @@ var typed = new Typed(".typed2", {
 
 
 
+const form = document.querySelector("#form");
+const submitForm = (e) => {
+  e.preventDefault();
+  const inputs = document.querySelectorAll('.validate');
+  const invalidFields = Array.from(inputs).filter(input => input.value === "");
+  if (invalidFields.length > 0) {
+    invalidFields.forEach((field) => {
+      field.classList.add('error')
+    })
+    return false;
+  } else {
+    form.submit();
+  }
+}
+form.addEventListener("submit", submitForm);
+
 
 
 // let form = document.querySelector("#formSubmit");
