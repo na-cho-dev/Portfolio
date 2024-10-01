f7dba25cf5a5d83eb3934f0512889f93c4a4a9e1
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-01 17:57:15 +0100

    Feature: Update on Theme Images Optimization

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // LOADER
-document.addEventListener("load", function () {
+document.addEventListener("DOMContentLoaded", function () {
   const imageUrls = [
     "https://res.cloudinary.com/nachodev/image/upload/laptop_white_ilz0q7.png",
     "https://res.cloudinary.com/nachodev/image/upload/laptop_black_cef644.jpg",
